8e5898a07445b89df2d4f48e6dec247621bddbaa
FalkovskiyAV <avf-91@yandex.ru>
2026-04-13 17:43:06 +0300

    Outlet

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,22 @@ import { Menu } from './pages/Menu/Menu';
 import { Cart } from './pages/Cart/Cart';
 import { Error } from './pages/Error/Error';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Layout } from './layout/Menu/Menu';
 
 const router = createBrowserRouter([
 	{
 		path: '/',
-		element: <Menu/>
-	},
-	{
-		path: '/cart',
-		element: <Cart/>
+		element: <Layout/>,
+		children: [
+			{
+				path: '/',
+				element: <Menu/>
+			},
+			{
+				path: '/cart',
+				element: <Cart/>
+			}
+		]
 	},
 	{
 		path: '*',
