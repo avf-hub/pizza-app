896960946f96b5f03365a39d23eb61eb373ee2c0
FalkovskiyAV <avf-91@yandex.ru>
2026-04-13 14:03:54 +0300

    Создание роутов

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { useState, type MouseEvent } from 'react';
 import Button from './components/Button/Button';
 import Input from './components/Input/Input';
+import { Route, Routes } from 'react-router-dom';
+import { Menu } from './pages/Menu/Menu';
+import { Cart } from './pages/Cart/Cart';
 
 function App() {
 	const [counter, setCounter] = useState<number>(0);
@@ -14,6 +17,14 @@ function App() {
 			<Button onClick={addCounter} >Кнопка {counter}</Button>
 			<Button appearence='big' onClick={addCounter} >Кнопка {counter}</Button>
 			<Input placeholder='Email'/>
+			<div>
+				<a href='/'>Меню</a>
+				<a href='/cart'>Корзина</a>
+			</div>
+			<Routes>
+				<Route path='/' element={<Menu/>} />
+				<Route path='/cart' element={<Cart/>} />
+			</Routes>
 		</>
 	);
 }
