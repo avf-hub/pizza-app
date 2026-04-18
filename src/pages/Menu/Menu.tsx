d2ff3620d10adedee735a8979d908412da618256
FalkovskiyAV <avf-91@yandex.ru>
2026-04-18 23:10:47 +0300

    Подключение axios

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -5,23 +5,32 @@ import Search from '../../components/Search/Search';
 import { PREFIX } from '../../helpers/API';
 import type { Product } from '../../interfaces/product.interface';
 import styles from './Menu.module.css';
+import axios from 'axios';
 
 export function Menu() {
 	const [products, setProducts] = useState<Product[]>([]);
 
 	const getMenu = async () => {
 		try {
-			const res = await fetch(`${PREFIX}/products`);
-			if (!res.ok) {
-				return;
-			}
-			const data:Product[] = await res.json();
+			const {data} = await axios.get<Product[]>(`${PREFIX}/products`);
 			setProducts(data);
 		}
 		catch (e) {
 			console.error(e);
 			return;
 		}
+	// 	try {
+	// 		const res = await fetch(`${PREFIX}/products`);
+	// 		if (!res.ok) {
+	// 			return;
+	// 		}
+	// 		const data:Product[] = await res.json();
+	// 		setProducts(data);
+	// 	}
+	// 	catch (e) {
+	// 		console.error(e);
+	// 		return;
+	// 	}
 	};
 
 	useEffect(() => {
