427ff004b9d4830cde7d97b3dfe5364c2bc79409
FalkovskiyAV <avf-91@yandex.ru>
2026-04-19 22:43:37 +0300

    errorElement

--- src/pages/Menu/MenuList/MenuList.tsx
+++ src/pages/Menu/MenuList/MenuList.tsx
@@ -1,18 +1,21 @@
 import ProductCard from '../../../components/ProductCard/ProductCard';
 import type { MenuListProps } from './MenuList.props';
+import styles from './MenuList.module.css';
 
 function MenuList({products}: MenuListProps) {
-	return products.map(prod => (
-		<ProductCard
-			key={prod.id}
-			id={prod.id}
-			title={prod.name}
-			description={prod.ingredients.join(', ')}
-			rating={prod.rating}
-			price={prod.price}
-			image={prod.image}
-		/>
-	));
+	return <div className={styles.wrapper}>
+		{products.map(prod => (
+			<ProductCard
+				key={prod.id}
+				id={prod.id}
+				title={prod.name}
+				description={prod.ingredients.join(', ')}
+				rating={prod.rating}
+				price={prod.price}
+				image={prod.image}
+			/>
+		))}
+	</div>;
 }
 
 export default MenuList;
