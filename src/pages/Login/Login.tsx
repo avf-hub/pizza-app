33b3a696cd0e826cb23dc84108e71cfd89a99965
FalkovskiyAV <avf-91@yandex.ru>
2026-04-25 00:03:36 +0300

    Форма логина

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,5 +1,34 @@
+import { Link } from 'react-router-dom';
+import Button from '../../components/Button/Button';
+import Headling from '../../components/Headling/Headling';
+import Input from '../../components/Input/Input';
+import styles from './Login.module.css';
+import type { SubmitEvent } from 'react';
+
 function Login() {
-	return <>Login</>;
+	const submit = (e: SubmitEvent) => {
+		e.preventDefault();
+		console.log(e);
+	};
+
+	return <div className={styles.login} onSubmit={submit}>
+		<Headling>Вход</Headling>
+		<form className={styles.form}>
+			<div className={styles.field}>
+				<label htmlFor="email">Ваш email</label>
+				<Input id="email" placeholder="Email"/>
+			</div>
+			<div className={styles.field}>
+				<label htmlFor="password">Ваш пароль</label>
+				<Input id="password" type="password" placeholder="Пароль"/>
+			</div>
+			<Button appearence='big'>Вход</Button>
+		</form>
+		<div className={styles.links}>
+			<div>Нет аккаунта?</div>
+			<Link to="/auth/register">Зарегистрироваться</Link>
+		</div>
+	</div>;
 }
 
 export default Login;
