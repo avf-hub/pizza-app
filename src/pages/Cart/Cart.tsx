070d505e0e63bcaa761dd10b223e59add014d3e2
FalkovskiyAV <avf-91@yandex.ru>
2026-05-06 13:44:19 +0300

    Расчёт суммы

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -8,9 +8,19 @@ import { PREFIX } from '../../helpers/API';
 import CartItem from '../../components/CartItem/CartItem';
 import styles from './Cart.module.css';
 
+const DELIVERY_FEE = 169;
+
 export function Cart() {
 	const [cardProducts, setCardProducts] = useState<Product[]>([]);
 	const items = useSelector((state: RootState) => state.cart.items);
+
+	const total = items.map(i => {
+		const product = cardProducts.find(prod => prod.id === i.id);
+		if (!product) {
+			return 0;
+		}
+		return i.count * product.price;
+	}).reduce((acc, i) => acc += i, 0);
 
 	useEffect(() => {
 		loadAllItems();
@@ -35,5 +45,19 @@ export function Cart() {
 			}
 			return <CartItem key={product.id} count={i.count} {...product} />;
 		})}
+		<div className={styles.line}>
+			<div className={styles.text}>Итог</div>
+			<div className={styles.price}>{total}&nbsp;<span>₽</span></div>
+		</div>
+		<hr className={styles.hr}/>
+		<div className={styles.line}>
+			<div className={styles.text}>Доставка</div>
+			<div className={styles.price}>{DELIVERY_FEE}&nbsp;<span>₽</span></div>
+		</div>
+		<hr className={styles.hr}/>
+		<div className={styles.line}>
+			<div className={styles.text}>Итог {items.length}</div>
+			<div className={styles.price}>{total * DELIVERY_FEE}&nbsp;<span>₽</span></div>
+		</div>
 	</>;
 }
